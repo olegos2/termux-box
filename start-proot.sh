f8dd493871c45e965081cf2d2b31224296ff259a
olegos2 <74909582+olegos2@users.noreply.github.com>
2023-06-14 20:08:47 +0500

    Update start-proot.sh

--- start-proot.sh
+++ start-proot.sh
@@ -1,3 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
+cd $(dirname $0)
 unset LD_PRELOAD
-proot --link2symlink -0 -r ubuntu-fs -b /dev -b /proc -b /sys -b ubuntu-fs/root:/dev/shm -b /sdcard -w /root /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM LANG=C.UTF-8 /bin/bash --login
+command="proot"
+command+=" --link2symlink -0"
+command+=" -r ubuntu-fs"   
+command+=" -b /dev -b /proc -b /sys -b ubuntu-fs/root:/dev/shm"
+command+=" -b /sdcard -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+exec $command
